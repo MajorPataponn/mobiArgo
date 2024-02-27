44ae34fa294887ca56da0e15612866672bdf4222
Major Pataponn <basti.postmuenster@yahoo.de>
2024-02-27 16:47:49 +0100

    Create mobiArgo.js

---
+++ mobiArgo.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         mobiArgo
+// @version      1.0
+// @description  A custom UI js script for Argo Workflows
+// @author       SK
+// @match        https://argo-workflows.*.quintet.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
